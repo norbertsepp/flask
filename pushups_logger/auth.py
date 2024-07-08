229d6180768267688f4733af9e499c3e6b441228
norbertsepp <norbert@sepp.hu>
2024-07-08 10:33:00 +0200

    a signup és login funkciók hozzáadása

--- pushups_logger/auth.py
+++ pushups_logger/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, request, redirect
 
 auth = Blueprint("auth", __name__)  
 
@@ -6,11 +6,32 @@ auth = Blueprint("auth", __name__)
 def signup():
     return render_template('signup.html')
 
+@auth.route('/signup', methods=["POST"])
+def signup_post():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    
+    print(email, name, password)
+    
+    return redirect(url_for('auth.login'))
+    
+    
 
 @auth.route('/login')
 def login():
     return render_template('login.html')
 
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+    
+    print(email, password)
+    
+    return redirect(url_for('main.profile'))
 
 @auth.route('/logout')
 def logout():
