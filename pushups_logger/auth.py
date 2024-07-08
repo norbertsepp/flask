94dae670da9f77d2982df37bf494e6e2bb3efc9f
norbertsepp <norbert@sepp.hu>
2024-07-08 16:03:18 +0200

    updated some files

--- pushups_logger/auth.py
+++ pushups_logger/auth.py
@@ -1,10 +1,15 @@
 from flask import Blueprint, render_template, url_for, request, redirect
+from werkzeug.security import generate_password_hash
+from .models import User
+from . import db
 
 auth = Blueprint("auth", __name__)  
+
 
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+
 
 @auth.route('/signup', methods=["POST"])
 def signup_post():
@@ -12,8 +17,16 @@ def signup_post():
     email = request.form.get("email")
     password = request.form.get("password")
     
-    print(email, name, password)
+    #print(email, name, password)
+    user = User.query.filter_by(email=email).first()
     
+    if user:
+        print("User already exists!")
+    else:
+        new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+        db.session.add(new_user)
+        db.session.commit() 
+        
     return redirect(url_for('auth.login'))
     
     
--- pushups_logger/models.py
+++ pushups_logger/models.py
@@ -1,6 +1,6 @@
-from __init__ import db
+from . import db
 
-class User(db.model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(20)) 
